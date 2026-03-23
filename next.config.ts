5eb10911cfc4663144aceb453f1e807cd3ad1a50
root <root@srv1427049.hstgr.cloud>
2026-03-23 03:17:15 +0000

    fix: no usar trailingSlash para que Next Image funcione (/ _next/image)

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // Se despliega detrás de Nginx en subpath
   basePath: "/tornirepuestos",
-  // Evita redirecciones raras con el slash final al navegar desde Nginx
-  trailingSlash: true,
   images: {
     remotePatterns: [
       {
